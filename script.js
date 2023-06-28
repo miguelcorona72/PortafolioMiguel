a2aeed1b662dd62c13fd7d18e086b0ebea1a403d
miguelcorona72 <119025822+miguelcorona72@users.noreply.github.com>
2023-06-28 07:09:51 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let menuVisible = false;
+//FUNCION QUE OLCULTA O MUESTRA EL MENU
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
